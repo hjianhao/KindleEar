016799bb0e60e7df884241db35863b2687d6562e
Jason Huang <hjianhao@hotmail.com>
2021-01-29 10:53:32 +0800

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-dev_appserver.py \
-    --host=0.0.0.0 \
-    --enable_host_checking=False \
-    --smtp_host=smtp.qq.com \
-    --smtp_port=587 \
-    --smtp_user=38233951@qq.com \
-    --smtp_password=xxx \
-    --smtp_allow_tls=True \
-    ./app.yaml ./module-worker.yaml
