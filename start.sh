819c4dac119bb4aac2cbaa886212627ee9d0882d
Jason Huang <hjianhao@hotmail.com>
2021-01-29 10:52:32 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ dev_appserver.py \
     --smtp_host=smtp.qq.com \
     --smtp_port=587 \
     --smtp_user=38233951@qq.com \
-    --smtp_password=hzhawuyklgescagg \
+    --smtp_password=xxx \
     --smtp_allow_tls=True \
     ./app.yaml ./module-worker.yaml
